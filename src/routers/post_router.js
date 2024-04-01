a5ebea3af0b7bc227f718da395d5b91c69106325
Huy Nguyen <huyy.802@gmail.com>
2024-04-01 23:28:44 +0700

    feat: CRUD Post

--- src/routers/post_router.js
+++ src/routers/post_router.js
@@ -3,14 +3,14 @@ const router = express.Router();
 
 import { PostController } from "../controllers/post_controller.js";
 
-PostRouter.post("/:userId", PostController.createPost); // POST /api/v1/posts/:userId
-PostRouter.patch("/:postId", PostController.updatePost); // PATCH /api/v1/posts/:postId
-PostRouter.delete("/:postId", PostController.deletePost); // DELETE /api/v1/posts/:postId
-PostRouter.get("/others/:userId", PostController.getAllOtherPost); // GET /api/v1/posts/others/:userId
-PostRouter.get("/", PostController.getAllPost); // GET /api/v1/posts
-PostRouter.get("/:postId", PostController.getPostById); // GET /api/v1/posts/:postId
-PostRouter.get("/users/:userId", PostController.getAllPostsOfUser); // GET /api/v1/posts/users/:userId
-PostRouter.get("/deleted/:userId", PostController.getAllPostsDelete); // GET /api/v1/posts/deleted/:userId
-PostRouter.get("/suggestText", PostController.suggestText); // GET /api/v1/posts/suggestText
+router.post("/:userId", PostController.createPost); // POST /api/v1/posts/:userId
+router.patch("/:postId", PostController.updatePost); // PATCH /api/v1/posts/:postId
+router.delete("/:postId", PostController.deletePost); // DELETE /api/v1/posts/:postId
+router.get("/others/:userId", PostController.getAllOtherPost); // GET /api/v1/posts/others/:userId
+router.get("/", PostController.getAllPost); // GET /api/v1/posts
+router.get("/:postId", PostController.getPostById); // GET /api/v1/posts/:postId
+router.get("/users/:userId", PostController.getAllPostsOfUser); // GET /api/v1/posts/users/:userId
+router.get("/deleted/:userId", PostController.getAllPostsDelete); // GET /api/v1/posts/deleted/:userId
+router.get("/suggestText", PostController.suggestText); // GET /api/v1/posts/suggestText
 
 export default router;
